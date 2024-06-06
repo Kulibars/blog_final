61e9865e84ca57ccea6223eaad351a46ed09f3e1
Kulibars <125401672+Kulibars@users.noreply.github.com>
2024-06-06 17:18:56 +0300

    add Dockerfile

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { Blog } from "./Blog";
+import { Blog } from "./blog";
 import "./index.css";
 import { store } from "./store";
 
